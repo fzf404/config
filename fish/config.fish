74538cc28e488d5437184d00bdcf5ad5a3ae00a1
fzf404 <me@fzf404.art>
2024-08-26 22:27:52 +0800

    feat: shell config

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,8 @@
-fish_add_path /opt/homebrew/bin /Users/fzf404/Library/pnpm
+fish_add_path /opt/homebrew/bin /Users/fzf404/.bun/bin /Users/fzf404/Library/pnpm
 
 fnm env | source
 zoxide init fish | source
 starship init fish | source
-eval conda 'shell.fish' hook $argv | source
-set -gx PNPM_HOME /Users/fzf404/Library/pnpm
 
 alias v='hx'
 alias c='bat'
@@ -12,42 +10,65 @@ alias t='bumpp'
 alias r='trash'
 alias l='ls -lah'
 
-alias bi="brew install"
-alias bif="brew info"
-alias b="brew install"
-alias bs="brew search"
-alias bu="brew upgrade --greedy"
+alias b="bun"
+alias bi="bun install"
+alias ba="bun add"
+alias bd="bun dev"
+alias bb="bun build"
+
+alias br="brew"
+alias bri="brew install"
+alias brui="brew uninstall"
+alias brl="brew list"
+alias brif="brew info"
+alias brs="brew search"
+alias bru="brew upgrade -g"
 
 alias n='pnpm'
 alias ny='pnpm why'
+alias np='pnpm publish'
+alias nlg='pnpm list -g'
+
 alias na='pnpm add'
 alias nad='pnpm add -D'
 alias nag='pnpm add -g'
+
+alias nr='pnpm rm'
+alias nrg='pnpm rm -g'
+
 alias ni='pnpm install'
+alias nir='pnpm install --recursive'
+
 alias nu='pnpm update'
 alias nul='pnpm update --latest --interactive'
-alias np='pnpm publish'
+
 alias nd='pnpm run dev'
-alias nt='pnpm run test'
+alias nrd='pnpm run -r dev'
+alias nb='pnpm run build'
+alias nrb='pnpm run -r build'
+alias ndp='pnpm run deploy'
+alias nrdp='pnpm run -r deploy'
+
 alias nl='pnpm run lint'
 alias nf='pnpm run format'
 alias nle='pnpm run lint:eslint'
 alias nlp='pnpm run lint:prettier'
 alias nls='pnpm run lint:stylelint'
+
+alias nt='pnpm run test'
 alias ns='pnpm run start'
-alias nb='pnpm run build'
 alias nck='pnpm run check'
-alias nr='pnpm run release'
+alias nrl='pnpm run release'
 alias npv='pnpm run preview'
 alias ng='pnpm run generate'
-alias ndp='pnpm run deploy'
+
 alias ntp='t && np'
 alias nbp='t -x "pnpm run build" && np'
 
 alias g='gitcz --disable-emoji'
 
 alias ga='git add -A'
-alias gc='git commit -m'
+alias gc='git clone'
 alias gb='git checkout --orphan'
 alias gp='git push'
 alias gpl='git pull'
@@ -59,6 +80,11 @@ alias gatp='ga && gctp'
 alias gabp='ga && gcbp'
 
 alias ghc='gh repo clone'
+
+alias u='/Applications/HBuilderX.app/Contents/MacOS/cli'
+alias ua='u cloud functions --upload all --prj BonjourApp --provider aliyun --force'
+alias uc='u cloud functions --upload common --prj BonjourApp --provider aliyun --force --name'
+alias ur='u cloud functions --upload cloudfunction --prj BonjourApp --provider aliyun --force --name'
 
 function qrc
     curl qrenco.de/$argv
@@ -80,3 +106,17 @@ function ghbot --argument owner
     test -n "$owner"; or set owner fzf404
     gh search prs --owner $owner is:open archived:false label:dependencies --json url --jq ".[].url" | xargs -I URL bash -c 'echo "Approving & Merging: URL" && gh pr review --approve URL && gh pr merge --squash --auto URL'
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
+    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+        . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
